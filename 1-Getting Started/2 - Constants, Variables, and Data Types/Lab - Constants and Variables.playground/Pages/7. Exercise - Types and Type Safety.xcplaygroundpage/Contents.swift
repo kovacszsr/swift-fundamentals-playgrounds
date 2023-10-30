3c14c194832ad3eec0c8fef2c47652ead1507a5d
kovacszsr <kovacs.zsofia.ramona98@gmail.com>
2023-10-30 18:41:32 +0100

    Initial commit

---
+++ 1-Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+/*:
+## Exercise - Types and Type Safety
+ 
+ Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
+ */
+
+var firstDecimal = 25.546
+var secondDecimal = 45.876
+
+//:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+
+var trueOrFalse = true
+//firstDecimal = trueOrFalse
+
+//:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+
+var oneString = "ab"
+
+//firstDecimal = oneString
+
+//:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+
+var oneInt = 5
+//firstDecimal = oneInt
+
+/*:
+[Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
+ */
