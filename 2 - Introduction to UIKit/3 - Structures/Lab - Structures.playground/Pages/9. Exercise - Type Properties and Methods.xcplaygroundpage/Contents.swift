3cf215fa193b8c3156eeb3ecc418141392fbe540
kovacszsr <kovacs.zsofia.ramona98@gmail.com>
2023-11-03 15:14:44 +0100

    Finished "Structures" playground

--- 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -4,12 +4,19 @@
  Imagine you have an app that requires the user to log in. You may have a `User` struct similar to that shown below. However, in addition to keeping track of specific user information, you might want to have a way of knowing who the current logged in user is. Create a `currentUser` type property on the `User` struct below and assign it to a `user` object representing you. Now you can access the current user through the `User` struct. Print out the properties of `currentUser`.
  */
 struct User {
+    
+    static var currentUser = User(userName: "kovacszsr", email: "user@example.com", age: 25)
         
     var userName: String
     var email: String
     var age: Int
+    
+    static func logIn(user: User) {
+        print("Hello \(user.userName). You are logged in.")
+    }
 }
 
+print(User.currentUser)
 
 /*:
  There are other properties and actions associated with a `User` struct that might be good candidates for a type property or method. One might be a method for logging in. Go back and create a type method called `logIn(user:)` where `user` is of type `User`. In the body of the method, assign the passed in user to the `currentUser` property, and print out a statement using the user's userName saying that the user has logged in.
@@ -17,6 +24,7 @@ struct User {
  Below, call the `logIn(user:)` method and pass in a different `User` instance than what you assigned to currentUser above. Observe the printout in the console.
  */
 
+User.logIn(user: User.currentUser)
 
 /*:
 [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Type Properties and Methods](@next)
